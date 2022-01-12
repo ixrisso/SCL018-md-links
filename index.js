7526e6c12c6073c10fa87067b85b92a5a781e6a3
Isabella X. Risso <ixrisso@gmail.com>
2022-01-12 08:33:43 -0300

    feature: first readings

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-module.exports = () => {
-  // ...
+import fs from 'fs';
+import path from 'path';
+import fetch from 'node-fetch';
+import { userPath,
+fileExtension,
+fileFolder,
+getLinks,
+ } from './api.js'
+
+//Función que une todo
+export const mdLinks = (userPath, option) => {
+  return new Promise((resolve, reject) => {
+    const links = getLinks(userPath);
+    if (option.validate) {
+      resolve(validateLinks(links));
+    } else {
+      resolve(links);
+    }
+    reject(new TypeError('error'));
+  });
 };
